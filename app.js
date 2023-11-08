943a45ede8de6631b509b3441982e076499c8f4f
sacha0914 <sacha0914.poidevin@gmail.com>
2023-11-08 14:28:46 +0100

    test

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World !')
-})
+const messages = ["React", "Vue", "Angular"];
+
+
+app.get('/data', (req, res) => {
+    res.send(messages);
+    
+});
+
+app.post('/data', (req, res) => {
+    let msg = req.body;
+    // res.send('Got a POST request')
+    console.log(msg);
+    messages.push(msg.name);
+    res.json(msg);
+    console.log(messages);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
+
+module.exports = app;
