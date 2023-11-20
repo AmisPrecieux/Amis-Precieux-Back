a8ccbb92d79c2a88dc2ed475d7e5a5bbcecf85e1
SACHA POIDEVIN <96061674+sacha0914@users.noreply.github.com>
2023-11-20 17:27:58 +0100

    Final Commit (#2)

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  mail: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = model("User", userSchema);
+
+export default User;
---
+++ router/auth.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import { getUser, setUser } from "../services/auth.js";
+import verifyToken from "../middleware/verifyToken.js";
+
+const authRouter = Router();
+
+authRouter.post("/signup", async (req, res) => {
+
+  if (!req.body.mail || !req.body.password) {
+    return res.status(400).send("Vous devez renseigner le nom et le mot de passe");
+  }
+
+  try {
+    await setUser(req.body);
+    return res.status(201).send("Compte créés");
+  } catch (error) {
+    return res.status(403).send(error.toString());
+  }
+});
+
+authRouter.post("/signin", async (req, res) => {
+
+  try {
+    const token = await getUser(req.body);
+    return res.status(201).send(token);
+  } catch (error) {
+
+    return res.status(403).send(error.toString());
+  }
+})
+
+authRouter.get("/test", verifyToken, (req, res) => {
+  return res.send("ça marche");
+})
+
+export default authRouter;
